f0e0379f964b3788a6e6eb48707840f000019b59
Jonathan Vazquez <johnvazna@gmail.com>
2021-03-09 20:16:12 -0600

    [UPDATE] Add login screen view

---
+++ RecyclerView/app/src/main/java/com/apprendev/recyclerview/LoginLayout.kt
@@ -0,0 +1,11 @@
+package com.apprendev.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_layout)
+    }
+}
