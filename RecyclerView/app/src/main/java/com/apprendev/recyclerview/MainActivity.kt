5f332dc306899c91010611a35352f904b68ce2a8
Jonathan Vazquez <johnvazna@gmail.com>
2021-03-09 20:48:23 -0600

    [UPDATE] Add navigation for the users

--- RecyclerView/app/src/main/java/com/apprendev/recyclerview/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/apprendev/recyclerview/MainActivity.kt
@@ -2,10 +2,41 @@ package com.apprendev.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.apprendev.recyclerview.fragments.FragmentHome
+import com.apprendev.recyclerview.fragments.FragmentSearch
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navBottom.setOnNavigationItemSelectedListener(this)
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_home -> changeFragment(FragmentHome())
+            R.id.nav_search -> changeFragment(FragmentSearch())
+        }
+
+        return true
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            FragmentTransaction.TRANSIT_FRAGMENT_OPEN
+            replace(R.id.container, fragment)
+            addToBackStack(null)
+            commit()
+        }
+    }
+
 }
